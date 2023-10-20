5b7609f9635636a91e31d6baaf2ea1f47bb6061e
pbxforce <120632049+pbxforce@users.noreply.github.com>
2023-10-20 12:27:58 +0530

    Update install-vpn.sh
    fixed PKI directory error

--- install-vpn.sh
+++ install-vpn.sh
@@ -575,7 +575,8 @@ else
 fi
 #Generating PKI directory
 yes yes | $rsaconf_dir/./easyrsa init-pki 2>/dev/null >/dev/null &
-ln -s $rsaconf_dir/pki $PWD 
+sleep 2
+ln -s $PWD/pki $rsaconf_dir 
 echo " "
 loading 'Creating PKI Directory for Cert. and Keys' 3
 #Verifying wether the directory is created or not
